9c93942f6f25c5d56bcc38e3149bc60e13cb5bcd
HANITRINIRINA SAROBIDY p2102840 <p2102840@b710l0612.univ-lyon1.fr>
2023-03-07 11:14:38 +0100

    classe dimension

--- src/Labyrinthe.h
+++ src/Labyrinthe.h
@@ -1,1 +1,16 @@
+#ifndef LABY_H
+#define LABY_H 
+#include "Dimension.h"
 
+class Labyrinthe {
+
+    private : 
+
+    enum TypeLab { VIDE = 0 , MUR = 1 ,EMPCIBLE= 2 };
+    Dimension dimL;
+    TypeLab tabLab[200][200];
+
+
+};
+
+#endif
--- src/Position.h
+++ src/Position.h
@@ -1,12 +1,28 @@
-#ifndef DIMENSION_H
-#define DIMENSION_H
 
-class dimension {
-    public:
-        float largeur;
-        float hauteur;
+#ifndef POSITION_H
+#define POSITION_H
+
+class Position {
+    private :
+    int x,y ;
+    public: 
+    Position();
+    ~Position();
+    int getPosX() const ;
+    int getPosY() const ;
+    void setPosX( int x );
+    void setPosY( int y);
+
+    
 
 
-}
 
-#endif
+
+
+};
+
+
+
+
+#endif 
+
