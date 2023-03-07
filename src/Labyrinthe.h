5659f429106461a188f83fd5b6a240d6e8412db1
ibtissam <you@example.com>
2023-03-07 11:27:52 +0100

    Modification classe Labyrinthe

--- src/Labyrinthe.h
+++ src/Labyrinthe.h
@@ -1,6 +1,7 @@
 #ifndef LABY_H
 #define LABY_H 
 #include "Dimension.h"
+#include "Position.h"
 
 class Labyrinthe {
 
@@ -8,7 +9,17 @@ class Labyrinthe {
 
     enum TypeLab { VIDE = 0 , MUR = 1 ,EMPCIBLE= 2 };
     Dimension dimL;
+
     TypeLab tabLab[200][200];
+
+    public: 
+    Labyrinthe();
+    ~Labyrinthe();
+    TypeLab getTypeLab (Position & p );
+    void setTypeLab(Position & p , TypeLab lab );
+    Dimension getDim();
+    void setDim(const Dimension & dim );
+
 
 
 };
