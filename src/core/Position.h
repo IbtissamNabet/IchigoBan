9b673cc4772de72653737e741340e3b5e58cbe22
ibtissam <you@example.com>
2023-03-07 11:41:45 +0100

    recréation

---
+++ src/core/Position.h
@@ -0,0 +1,28 @@
+
+#ifndef POSITION_H
+#define POSITION_H
+
+class Position {
+    private :
+    int x,y ;
+    public: 
+    Position();
+    ~Position();
+    int getPosX() const ;
+    int getPosY() const ;
+    void setPosX( int x );
+    void setPosY( int y);
+
+    
+
+
+
+
+
+};
+
+
+
+
+#endif 
+
