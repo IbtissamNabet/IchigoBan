633239c7662a7b2e3d03da50b0a03f54bccb10fc
NABET IBTISSAM p2208660 <ibtissam.nabet@etu.univ-lyon1.fr>
2023-03-07 08:19:45 +0000

    Update src/Fraise.cpp

--- src/Fraise.cpp
+++ src/Fraise.cpp
@@ -1,1 +1,17 @@
+class Fraise {
+    private :
+    pos_fraise:Position 
+
+    public:
+    Fraise();
+    ~Fraise();
+    Position get_Position_Fraise();
+    void set_Position_Fraise();
+    bool positionValide(const & Labyrinthe);
+    void gauche(const & Labyrinthe);
+    void droit(const & Labyrinthe);
+    void haut(const & Labyrinthe);
+    void bas(const & Labyrinthe);
+
+
 
