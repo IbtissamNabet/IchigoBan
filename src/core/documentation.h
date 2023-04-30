bcacf4a3a5f8c42ac4f68e1273405cad30ba3d04
Hanitrinirina Sarobidy <sarobidy.hanitrinirina@etu.univ-lyon1.fr>
2023-04-30 21:37:46 +0200

    Doc

---
+++ src/core/documentation.h
@@ -0,0 +1,16 @@
+/** 
+ * @mainpage IchigoBan 
+ * @section intro Introduction
+ * Jeu inspiré de Sokoban
+ * développé par
+ * @author ibtissam.nabet@etu.univ-lyon1.fr ,sarobidy.hanitrinirina@etu.univ-lyon1.fr 
+ * @date 29/04/23
+ * @section Langage 
+ * Code écrit en C++
+ * @section Compilation 
+ * Se compile avec g++
+ * @section Execution Pour executer
+ * $ ./bin/pacman_txt <br>
+ * ou <br>
+ * $ ./bin/pacman_sdl <br>
+ */
